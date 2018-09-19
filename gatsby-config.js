67fa0e8fd8fae5590b388ea916fec205ebb21609
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2018-09-18 23:32:37 -0500

    completes lesson 1

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'React For Designers',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,12 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Learn to design and code React apps</h1>
+    <p>
+      Complete courses about the best tools and design systems. Prototype and
+      build apps with React and Swift.
+    </p>
+    <Link to="/page-2/">Watch the video</Link>
   </Layout>
 )
 
